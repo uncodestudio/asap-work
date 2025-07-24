5a65aa48c8e7d46dcaa8ff1156e064e5353752e9
starkad57 <fabien@uncode-studio.com>
2025-07-24 12:07:29 +0400

    add splide success to home

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const pages = {
-  'homepage': ['faq-accordion','marquee-slider','process-scroll','splide-testimonies','text-animation','text-translate','faq-filters'],
+  'homepage': ['faq-accordion','marquee-slider','process-scroll','splide-testimonies','text-animation','text-translate','faq-filters','splide-success'],
   'product': ['product-scroll','faq-accordion','faq-filters'],
   'articles':['other-articles'],
   'contact':['splide-testimonies'],
